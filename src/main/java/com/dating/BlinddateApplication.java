5889ab4d883ca71fb16a4abecf16e9554541ece7
Lion-tang <361092772@qq.com>
2020-09-14 09:28:21 +0800

    inital

---
+++ src/main/java/com/dating/BlinddateApplication.java
@@ -0,0 +1,13 @@
+package com.dating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinddateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlinddateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dating/BlinddateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinddateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
