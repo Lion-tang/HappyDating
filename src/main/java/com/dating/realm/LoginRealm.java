eff0505253d09777c7419307d6ed940f2275ed1b
up2Kino <781022552@qq.com>
2020-09-15 09:53:16 +0800

    release

--- src/main/java/com/dating/realm/LoginRealm.java
+++ src/main/java/com/dating/realm/LoginRealm.java
@@ -6,23 +6,14 @@ import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.springframework.beans.factory.annotation.Autowired;
 
-<<<<<<< Updated upstream:src/main/java/com/dating/realm/LoginRealm.java
-public class LoginRealm extends AuthorizingRealm {
-
-    @Autowired
-    private UserService userService;
-=======
 import javax.annotation.Resource;
-import java.util.Map;
 
 
 public class LoginRealm extends AuthorizingRealm {
     @Resource
     private UserService userService;
 
->>>>>>> Stashed changes:src/main/java/com/dating/Realm/LoginRealm.java
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
         return null;
@@ -32,13 +23,8 @@ public class LoginRealm extends AuthorizingRealm {
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
         User user = userService.findByUserName(token.getUsername());
-<<<<<<< Updated upstream:src/main/java/com/dating/realm/LoginRealm.java
         if(user != null){
             return new SimpleAuthenticationInfo(user,user.getPassword(),getName());
-=======
-        if (user != null) {
-            return new SimpleAuthenticationInfo(user, user.getPassword(), getName());
->>>>>>> Stashed changes:src/main/java/com/dating/Realm/LoginRealm.java
         }
         return null;
     }
